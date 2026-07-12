f37fa1e2db348c25d74ed6b06127a81bf651a305
cggruda069-maker <cggruda069@gmail.com>
2026-07-12 04:24:46 -0600

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,3 +1,4 @@
 import streamlit as st
 import google.generativeai as genai
 GEMINI_API_KEY = "AQ.Ab8RN6Jt065HaQQag4VCIc9L5JylX_DpL9kJgPeYTn4U7eMm5Q"
+model = genai.GenerativeModel("gemini-2.5-flash")
